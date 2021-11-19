dc4237f5f1598daa1e3b4b3e0ec862b7c835a1ac
andrew <andrewoliveira282@gmail>
2021-11-19 20:29:46 -0300

    SellerDaoJDBC e DaoFactory

--- demo_dao/src/application/program.java
+++ demo_dao/src/application/program.java
@@ -2,6 +2,8 @@ package application;
 
 import enditities.Departament;
 import enditities.Seller;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 
 import java.util.Date;
 
@@ -9,7 +11,7 @@ public class program {
     public static void main(String[] args) {
         Departament obj = new Departament(1,"books");
         Seller seller = new Seller(21,"bob","bob@gmail.com",new Date(),3000.0,obj);
-
+        SellerDao sellerDao =  DaoFactory.createSellerDao();
         System.out.println(seller);
     }
 }
---
+++ demo_dao/src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,34 @@
+package model.dao.impl;
+
+import enditities.Seller;
+import model.dao.SellerDao;
+
+import java.util.List;
+
+public class SellerDaoJDBC implements SellerDao {
+
+    @Override
+    public void insert(Seller obj) {
+
+    }
+
+    @Override
+    public void uptade(Seller obj) {
+
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
+    @Override
+    public Seller findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        return null;
+    }
+}
